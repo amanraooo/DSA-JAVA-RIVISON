4b67e83ff377bda75f66aa4db489a597a747f611
amanraooo <ak9070777@gmail.com>
2025-08-14 09:39:25 +0530

    commit

--- Arrays/2D-Array/SpiralPrint.java
+++ Arrays/2D-Array/SpiralPrint.java
@@ -1,5 +1,46 @@
 public class SpiralPrint {
     public static void main(String[] args) {
-        
+        int mat[][] = { { 1, 2, 3, 4 },
+                        { 5, 6, 7, 8 },
+                        { 9, 10, 11, 12 }
+        };
+        spiral(mat);
+    }
+    public static void spiral(int mat[][]){
+        int min_r = 0;
+        int max_r = mat.length-1;
+        int min_c = 0 ;
+        int max_c = mat[0].length-1;
+
+        int count = 0;
+        int max = mat.length * mat[0].length;
+
+        while(min_r<=max_r && min_c <= max_c){
+            for (int r = min_r; r <= max_r && count<=max; r++) {
+                System.out.print(mat[r][min_c]+" ");
+                count++;
+            }
+            System.out.println();
+            for (int c = min_c+1; c <= max_c && count<=max; c++) {
+                System.out.print(mat[max_r][c]+ " ");
+                count++;
+            }
+            System.out.println();
+
+            for (int r = max_r-1; r >= min_r && count<=max; r--) {
+                System.out.print(mat[r][max_c]+" ");
+                count++;
+            }
+            System.out.println();
+
+            for (int c = max_c-1; c >= min_c+1 && count<=max; c--) {
+                System.out.print(mat[min_r][c]+ " ");
+                count++;
+            }
+            System.out.println();
+
+           min_r++; min_c++;
+           max_r--; max_c--;
+        }
     }
 }
