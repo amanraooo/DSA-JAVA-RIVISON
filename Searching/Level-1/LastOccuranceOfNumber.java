bd68a1c8c93cd589be8d1a7071e34565374c1d31
amanraooo <ak9070777@gmail.com>
2025-08-15 13:32:48 +0530

    Binary search

---
+++ Searching/Level-1/LastOccuranceOfNumber.java
@@ -0,0 +1,29 @@
+public class LastOccuranceOfNumber {
+    public static void main(String[] args) {
+        int nums[] = { 10, 20, 20, 20, 30, 40, 50, 60, 70, 80, 90 };
+        int result = LO(nums, 20);
+        System.out.println("The last occurance of is at " + result);
+    }
+
+    public static int LO(int nums[], int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        int found = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                found = mid;
+                start = mid + 1;
+
+
+            } else if (nums[mid] < target)
+                start = mid + 1;
+
+            else if (nums[mid] > target)
+                end = mid - 1;
+        }
+        return found;
+
+    }
+}
