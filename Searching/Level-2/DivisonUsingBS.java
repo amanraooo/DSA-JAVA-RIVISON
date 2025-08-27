6dc9525ef8b48834c7a4f8e2094c79ffcca9fa42
amanraooo <ak9070777@gmail.com>
2025-08-27 12:55:49 +0530

    BS Divison

---
+++ Searching/Level-2/DivisonUsingBS.java
@@ -0,0 +1,25 @@
+public class DivisonUsingBS {
+    public static void main(String[] args) {
+        System.out.println("Result: " + find(7, 31));
+    }
+
+    public static int find(int Divisor, int Dividend) {
+        int s = 0;
+        int e = Dividend;
+        int ans = -1;
+
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (mid * Divisor == Dividend) {
+                return mid;
+            }
+            if (mid * Divisor > Dividend) {
+                e = mid - 1;
+            } else {
+                ans = mid;
+                s = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
