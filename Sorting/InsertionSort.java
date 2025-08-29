fa63087b73cd61b9d18277cd0f2a05692bcd45d2
amanraooo <ak9070777@gmail.com>
2025-08-29 17:31:09 +0530

    insertion sort

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -1,23 +1,23 @@
-
-
 public class InsertionSort {
     public static void main(String[] args) {
-        int arr[] ={85,0,99,3,-2,-1};
+        int arr[] = { 85, 0, 99, 3, -2, -1 };
         sort(arr);
         printArr(arr);
     }
-    public static void sort(int arr[]){
+
+    public static void sort(int arr[]) {
         int n = arr.length;
         for (int i = 0; i < n; i++) {
             int key = arr[i];
-            int j = i-1;
-            while (j>=0 && arr[j]>key) {
-                arr[j+1] = arr[j];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
                 j--;
             }
-            arr[j+1] = key;
+            arr[j + 1] = key;
         }
     }
+
     public static void printArr(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
