a66c6193fb5ac4ddfba7d9bf9cf3de3142790c53
amanraooo <ak9070777@gmail.com>
2025-08-29 15:29:25 +0530

    selection sort

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,29 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        int arr[] = { 5, 4, 2, 0, 1 };
+        sort(arr);
+        printArr(arr);
+    }
+
+    public static void sort(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j <= n - 1; j++) {
+                if (arr[min] > arr[j]) {
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
