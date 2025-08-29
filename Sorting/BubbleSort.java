3b043ecf5f72cee187c33baad767ccc70a3c87d0
amanraooo <ak9070777@gmail.com>
2025-08-29 14:38:38 +0530

    bubble sort

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,28 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int arr1[] = { 5, 4, 3, 2, 1 };
+        sort(arr1);
+        printArr(arr1);
+    }
+
+    public static void sort(int arr[]) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
