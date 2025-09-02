fef28b57f2775048bb8952c56f98a474230ba03a
amanraooo <ak9070777@gmail.com>
2025-09-02 14:56:37 +0530

    insertion sort

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -7,7 +7,7 @@ public class InsertionSort {
 
     public static void sort(int arr[]) {
         int n = arr.length;
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i < n; i++) {
             int key = arr[i];
             int j = i - 1;
             while (j >= 0 && arr[j] > key) {
