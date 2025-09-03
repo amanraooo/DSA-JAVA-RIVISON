a4ef74d03753a9d66560bc50d881ea7d273090b1
amanraooo <ak9070777@gmail.com>
2025-09-03 15:52:44 +0530

    k-diff pairs

--- Searching/More questions/KDiffPairs.java
+++ Searching/More questions/KDiffPairs.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class KDiffPairs {
     public static void main(String[] args) {
@@ -7,18 +8,54 @@ public class KDiffPairs {
         System.out.println("result " + find(arr, 0));
     }
 
-    public static int find(int arr[], int k) {
+    // binary search apparoach
+    public static int findPairs(int arr[], int k){
+        return 0;
+    }
+
+    public static int BSApproach(int arr[], int k){
+        Arrays.sort(arr);
+        HashSet<String> ans = new HashSet<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if(bs(arr, i+1, arr[i]+k) != -1){
+                ans.add(arr[i]+ " , "+ arr[i]+ k);
+            }
+        }
+        return ans.size();
+
+    }
+
+    public static int bs(int nums[], int start, int target) {
+
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (target > nums[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    // two pointer aproach
+        public static int find(int arr[], int k) {
         Arrays.sort(arr);
 
         int i = 0;
         int j = 1;
-        int ans = 0;
+        
+        HashSet<String> ans = new HashSet<>();
         while (j < arr.length) {
 
             int diff = arr[j] - arr[i];
 
             if (diff == k) {
-                ans++;
+                 ans.add(arr[i] + ", " + arr[j]);
                 i++;
                 j++;
             } else if (diff > k) {
@@ -30,6 +67,6 @@ public class KDiffPairs {
                 j++;
             }
         }
-        return ans;
+        return ans.size();
     }
 }
