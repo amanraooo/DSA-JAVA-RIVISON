d628112ea1e44f831423c66c619ab685e55165b1
amanraooo <ak9070777@gmail.com>
2025-09-12 23:49:19 +0530

    better approach

---
+++ Searching/More questions/ExponentialSearchBetterApproach.java
@@ -0,0 +1,32 @@
+public class ExponentialSearchBetterApproach {
+    public static void main(String[] args) {
+        int arr[] = { 3, 4, 5, 6, 11, 13, 14, 15, 56, 70 }, x = 11;
+        System.out.println(search(arr, x));
+    }
+    public static int search(int arr[], int x){
+        int i = 0;
+        int j = 1;
+         int n = arr.length;
+
+        while(j<n && arr[j]<=x){
+            i = j;
+            j = j * 2;
+        }
+        return binarySearch(arr, x, i, Math.min(j, n - 1));
+    }
+
+    public static int binarySearch(int arr[], int x, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == x) {
+                return mid;
+            } else if (arr[mid] < x) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
