29e31771e64ff5a0a4438db5b4a18b99067bde4e
amanraooo <ak9070777@gmail.com>
2025-09-28 17:59:23 +0530

    removing adjacent duplicates

--- String/Level-1/RemoveAdjacentDupes.java
+++ String/Level-1/RemoveAdjacentDupes.java
@@ -3,19 +3,24 @@ public class RemoveAdjacentDupes {
     public static void main(String[] args) {
         String s1 = "abbaca";
         System.out.println(removeDuplicates(s1));
+
         String s2 = "azxxzy";
         System.out.println(removeDuplicates(s2));
+        
         String s3 = "aamany";
         System.out.println(removeDuplicates(s3));
     }
-    public static String removeDuplicates(String s){
+
+    public static String removeDuplicates(String s) {
         StringBuilder ans = new StringBuilder();
+
         int i = 0;
-        while (i<s.length()) {
-            if(ans.length()>0 && ans.charAt(ans.length()-1)== s.charAt(i) ){
-                ans.deleteCharAt(ans.length()-1);
+
+        while (i < s.length()) {
+            if (ans.length() > 0 && ans.charAt(ans.length() - 1) == s.charAt(i)) {
+                ans.deleteCharAt(ans.length() - 1);
             }
-            else{
+            else {
                 ans.append(s.charAt(i));
             }
             i++;
