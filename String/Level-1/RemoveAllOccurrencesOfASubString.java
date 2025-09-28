99dde24197de7edef704864b6269609483436703
amanraooo <ak9070777@gmail.com>
2025-09-28 18:20:52 +0530

    Remove All Occurrences Of A SubString

---
+++ String/Level-1/RemoveAllOccurrencesOfASubString.java
@@ -0,0 +1,13 @@
+
+public class RemoveAllOccurrencesOfASubString {
+    public static void main(String[] args) {
+         String s1 = "daabcbaabcbc", part1 = "abc";
+        System.out.println(removeOccurrences(s1, part1));
+    }
+    public static String removeOccurrences(String s, String part){
+        while(s.contains(part)){
+            s = s.replaceFirst(part,"");
+        }
+        return s;
+    }
+}
