4ee84f71e53bae8fe8db5d20fe32059ff026b464
amanraooo <ak9070777@gmail.com>
2025-10-04 15:30:19 +0530

    find & replace pattern

--- String/Level-2/FindAndReplacePattern.java
+++ String/Level-2/FindAndReplacePattern.java
@@ -7,28 +7,29 @@ public class FindAndReplacePattern {
         System.out.println(findAndReplacePattern(words1, pattern1));
     }
 
-    public static void createAndUpdateMapping(StringBuilder s){
+    public static void createAndUpdateMapping(StringBuilder s) {
         char start = 'a';
-        char mapping [] = new char[300];
+        char mapping[] = new char[300];
 
-        //mapping the string with abcs
+        // mapping the string with abcs
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            if(mapping[ch]==0){
-                mapping[ch]= start;
+            if (mapping[ch] == 0) {
+                mapping[ch] = start;
                 start++;
             }
         }
 
-        //update the og string with mapping chars
+        // update the og string with mapping chars
 
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
             s.setCharAt(i, mapping[ch]);
         }
-        
+
     }
-     public static List<String> findAndReplacePattern(String[] words, String pattern) {
+
+    public static List<String> findAndReplacePattern(String[] words, String pattern) {
         List<String> ans = new ArrayList<>();
         // firstly normalise the pattern
         StringBuilder Pattern = new StringBuilder(pattern);
