a89d2218e2b6381b7ac697ae5e73687feca7edf7
amanraooo <ak9070777@gmail.com>
2025-10-11 18:37:17 +0530

    majority element

--- some-extra- questions/MajorityElement.java
+++ some-extra- questions/MajorityElement.java
@@ -1,19 +1,20 @@
 public class MajorityElement {
     public static void main(String[] args) {
-        int nums[] = {2,2,1,1,1,2,2};
+        int nums[] = { 2, 2, 1, 1, 1, 2, 2 };
         System.out.println(MJ1(nums));
 
     }
-    public static int MJ1(int arr[]){
+
+    public static int MJ1(int arr[]) {
         int n = arr.length;
-        for(int i = 0; i<n ; i++){
+        for (int i = 0; i < n; i++) {
             int count = 0;
-            for(int j =0; j<n; j++){
-                if(arr[j]==arr[i]){
+            for (int j = 0; j < n; j++) {
+                if (arr[j] == arr[i]) {
                     count++;
                 }
             }
-            if(count > n/2){
+            if (count > n / 2) {
                 return arr[i];
             }
         }
