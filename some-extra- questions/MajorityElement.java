7bfa8db8217ea2daa13e1418f70be5cecc3dd0db
amanraooo <ak9070777@gmail.com>
2025-10-11 18:58:19 +0530

    majority element

--- some-extra- questions/MajorityElement.java
+++ some-extra- questions/MajorityElement.java
@@ -1,10 +1,14 @@
+import java.util.Arrays;
+
 public class MajorityElement {
     public static void main(String[] args) {
         int nums[] = { 2, 2, 1, 1, 1, 2, 2 };
-        System.out.println(MJ1(nums));
+        // System.out.println(MJ1(nums));
+        System.out.println(MJ2(nums));
 
     }
 
+    // brute force approach O(n2)
     public static int MJ1(int arr[]) {
         int n = arr.length;
         for (int i = 0; i < n; i++) {
@@ -16,6 +20,28 @@ public class MajorityElement {
             }
             if (count > n / 2) {
                 return arr[i];
+            }
+        }
+        return -1;
+    }
+
+    //optimized approach than brute force O(nlogn)
+    public static int MJ2(int arr[]) {
+        Arrays.sort(arr);
+        int n = arr.length;
+        int count = 1;
+        int ans = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] == arr[i - 1]) {
+                count++;
+            } else {
+                count = 1;
+                ans = arr[i];
+            }
+
+            if (count > n / 2) {
+                return ans;
             }
         }
         return -1;
