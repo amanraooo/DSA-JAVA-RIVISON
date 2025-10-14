81cba66e851933257b3f8e31b17af1b6dd554d1b
amanraooo <ak9070777@gmail.com>
2025-10-14 14:10:03 +0530

    valid anagram

--- String/More-Questions/ValidAnagram.java
+++ String/More-Questions/ValidAnagram.java
@@ -5,27 +5,27 @@ public class ValidAnagram {
         String s = "anagram", t = "nagaram";
         String s2 = "rat", t2 = "car";
 
-        System.out.println(isAnagram1(s,t));
-        System.out.println(isAnagram1(s2,t2));
+        System.out.println(isAnagram1(s, t));
+        System.out.println(isAnagram1(s2, t2));
 
-        System.out.println(isAnagram2(s,t));
-        System.out.println(isAnagram2(s2,t2));
-
+        System.out.println(isAnagram2(s, t));
+        System.out.println(isAnagram2(s2, t2));
 
     }
-    //brute force O(nlogn)
-    public static boolean isAnagram1(String s, String t){
+
+    // brute force O(nlogn)
+    public static boolean isAnagram1(String s, String t) {
         char sChar[] = s.toCharArray();
         char tChar[] = t.toCharArray();
 
         Arrays.sort(sChar);
         Arrays.sort(tChar);
 
-        return Arrays.equals(sChar,tChar);        
+        return Arrays.equals(sChar, tChar);
 
     }
 
-    //optimal solution O(n+m)
+    // optimal solution O(n+m)
     public static boolean isAnagram2(String s, String t) {
         int freqTable[] = new int[256];
 
