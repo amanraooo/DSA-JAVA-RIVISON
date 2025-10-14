5d7f05aae85086f9f6ff7f6fb0cee0fa0dfd44ff
amanraooo <ak9070777@gmail.com>
2025-10-14 15:41:30 +0530

    reverse only letters

---
+++ String/More-Questions/ReverseOnlyLetters.java
@@ -0,0 +1,34 @@
+
+public class ReverseOnlyLetters {
+    public static void main(String[] args) {
+        String s1 = "ab-cd";
+        String s2 = "a-bC-dEf-ghIj";
+        String s3 = "Test1ng-Leet=code-Q!";
+
+        System.out.println(revOnlyLetters(s1));
+        System.out.println(revOnlyLetters(s2));
+        System.out.println(revOnlyLetters(s3));
+    }
+    public static String revOnlyLetters(String s){
+        int l = 0;
+        int h  = s.length()-1;
+        char charArr[] = s.toCharArray();
+
+        while(l<h){
+            if(Character.isAlphabetic(charArr[l]) && Character.isAlphabetic(charArr[h])){
+                char temp = charArr[l];
+                charArr[l] = charArr[h];
+                charArr[h] = temp;
+                l++; 
+                h--;
+            }
+            else if(!Character.isAlphabetic(charArr[l])){
+                l++;
+            }
+            else{
+                h--;
+            }
+        }
+        return String.valueOf(charArr);
+    }
+}
