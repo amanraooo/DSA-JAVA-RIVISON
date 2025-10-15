c928afbba708cd30bda45f070fda822b8ea7d40f
amanraooo <ak9070777@gmail.com>
2025-10-15 15:26:12 +0530

    is Isomorphic string

--- String/Level-2/IsomorphicString.java
+++ String/Level-2/IsomorphicString.java
@@ -1,25 +1,25 @@
 public class IsomorphicString {
     public static void main(String[] args) {
-         String s1 = "add", t1 = "egg";
+        String s1 = "add", t1 = "egg";
         String s2 = "foo", t2 = "bar";
 
         System.out.println(isIsomorphic(s1, t1));
         System.out.println(isIsomorphic(s2, t2));
     }
 
-    public static boolean isIsomorphic(String s, String t){
-        int hash  [] = new int[256];
-        boolean isTmapped[]= new boolean[256];
-        
+    public static boolean isIsomorphic(String s, String t) {
+        int hash[] = new int[256];
+        boolean isTmapped[] = new boolean[256];
+
         for (int i = 0; i < s.length(); i++) {
-            if(hash[s.charAt(i)]==0 && isTmapped[t.charAt(i)]==false){
-                hash[s.charAt(i)]= t.charAt(i);
-                isTmapped[t.charAt(i)]=true;
+            if (hash[s.charAt(i)] == 0 && isTmapped[t.charAt(i)] == false) {
+                hash[s.charAt(i)] = t.charAt(i);
+                isTmapped[t.charAt(i)] = true;
             }
         }
 
         for (int i = 0; i < s.length(); i++) {
-            if(hash[s.charAt(i)] != t.charAt(i)){
+            if (hash[s.charAt(i)] != t.charAt(i)) {
                 return false;
             }
         }
