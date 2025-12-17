863398914e37d5465e80df1c1ab87f49ac223955
amanraooo <ak9070777@gmail.com>
2025-12-17 17:36:40 +0530

    practise

---
+++ Arrays/basics-and-practices/TwoSum.java
@@ -0,0 +1,29 @@
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int arr[] ={2,6,7,8,10};
+        int target = 33;
+        System.out.println(Tsum(arr,target));
+        
+
+
+    }
+    public static boolean Tsum(int arr[], int target){
+        int l =0 ;
+        int h = arr.length-1;
+        while (l<h) {
+            int sum = arr[l]+arr[h];
+            if(sum== target){
+        return true;
+            }
+        else if(sum > target)
+         h--;
+
+        else if (sum < target) l++;
+
+
+            }
+            return false;
+        }
+    }
+
