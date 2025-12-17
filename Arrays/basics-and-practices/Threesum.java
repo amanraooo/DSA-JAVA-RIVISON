374d1c8635b8779447547a4398706c59ae53262e
amanraooo <ak9070777@gmail.com>
2025-12-17 22:52:39 +0530

    practise

--- Arrays/basics-and-practices/Threesum.java
+++ Arrays/basics-and-practices/Threesum.java
@@ -5,18 +5,17 @@ import java.util.List;
 public class Threesum {
     public static void main(String[] args) {
         int arr[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        //ThreeSum(arr, 12);
-        int arr2[] = { -1,0,1,2,-1,-4 };
-        //ThreeSum(arr2, 0);
-         System.out.println(threeSum(arr2));
-
+        // ThreeSum(arr, 12);
+        int arr2[] = { -1, 0, 1, 2, -1, -4 };
+        // ThreeSum(arr2, 0);
+        System.out.println(threeSum(arr2));
 
     }
 
     public static void ThreeSum(int arr[], int target) {
-     Arrays.sort(arr);
+        Arrays.sort(arr);
         for (int anchor = 0; anchor < arr.length - 2; anchor++) {
-if (anchor > 0 && arr[anchor] == arr[anchor - 1])
+            if (anchor > 0 && arr[anchor] == arr[anchor - 1])
                 continue;
             int l = anchor + 1;
             int h = arr.length - 1;
@@ -27,8 +26,10 @@ if (anchor > 0 && arr[anchor] == arr[anchor - 1])
                     System.out.println(arr[anchor] + " , " + arr[l] + " , " + arr[h]);
                     l++;
                     h--;
-                    while (l < h && arr[l] == arr[l - 1]) l++;
-                    while (l < h && arr[h] == arr[h + 1]) h--;
+                    while (l < h && arr[l] == arr[l - 1])
+                        l++;
+                    while (l < h && arr[h] == arr[h + 1])
+                        h--;
                 } else if (threesum > target)
                     h--;
 
@@ -40,6 +41,7 @@ if (anchor > 0 && arr[anchor] == arr[anchor - 1])
         }
 
     }
+
     public static List<List<Integer>> threeSum(int[] nums) {
 
         List<List<Integer>> result = new ArrayList<>();
@@ -65,8 +67,10 @@ if (anchor > 0 && arr[anchor] == arr[anchor - 1])
                     l++;
                     h--;
 
-                    while (l < h && nums[l] == nums[l - 1]) l++;
-                    while (l < h && nums[h] == nums[h + 1]) h--;
+                    while (l < h && nums[l] == nums[l - 1])
+                        l++;
+                    while (l < h && nums[h] == nums[h + 1])
+                        h--;
 
                 } else if (sum < 0) {
                     l++;
