2e507e4b6f3600048b6d5a253b7ce5bbc9d931b8
amanraooo <ak9070777@gmail.com>
2025-12-17 16:56:29 +0530

    practise

---
+++ Arrays/basics-and-practices/CheckIfSorted.java
@@ -0,0 +1,27 @@
+public class CheckIfSorted {
+    public static void main(String[] args) {
+        int arr[] = {2,4,8,10,33};
+       System.out.println(ifSorted(arr));
+
+        int arr2[] = {33,10,6,4,1};
+       System.out.println(ifSorted(arr2));
+
+       int arr3[] = {10,33,4,2};
+       System.out.println(ifSorted(arr3));
+
+
+    }
+    public static String ifSorted(int arr[]){
+        boolean asc = true;
+        boolean desc = true;
+
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i]>arr[i-1]) desc = false;
+            else if(arr[i]<arr[i-1]) asc = false;
+        }
+        if(asc) return "Ascending";
+        if(desc) return "Descending";
+        return "not sorted";
+
+    }
+}
