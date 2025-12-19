9b2c435385f2e2b8140736fd0d25adf18ae8abef
amanraooo <ak9070777@gmail.com>
2025-12-19 23:26:53 +0530

    practise

--- Arrays/basics-and-practices/RotateAnArr.java
+++ Arrays/basics-and-practices/RotateAnArr.java
@@ -2,7 +2,10 @@ public class RotateAnArr {
     public static void main(String[] args) {
         int arr[] = { 10, 20, 30, 40, 50 };
         // shiftToRight(arr);
-        shiftToLeft(arr);
+        //shiftToLeft(arr);
+        //rotateByK(arr, 2);
+        rotateByK2(arr, 2);
+
     }
 
     public static void shiftToRight(int arr[]) {
@@ -25,6 +28,41 @@ public class RotateAnArr {
 
         display(arr);
 
+    }
+
+    public static void rotateByK(int arr[], int k){
+        for (int i = 0; i < k; i++) {
+            int temp = arr[arr.length - 1];
+        for (int j = arr.length - 1; j > 0; j--) {
+            arr[j] = arr[j - 1];
+        }
+        arr[0] = temp;
+        }
+        display(arr);
+    }
+
+    public static void rotateByK2(int arr[], int k){
+         k = k % arr.length;
+
+         ReverseArr(arr, 0, arr.length-1);
+         ReverseArr(arr, 0, k-1);
+         ReverseArr(arr, k, arr.length-1);
+
+        display(arr);
+
+
+    }
+
+    public static void ReverseArr(int arr[], int L, int R) {
+        
+        while (L < R) {
+            int temp = arr[L];
+            arr[L] = arr[R];
+            arr[R] = temp;
+            L++;
+            R--;
+        }
+        
     }
 
     public static void display(int arr[]) {
