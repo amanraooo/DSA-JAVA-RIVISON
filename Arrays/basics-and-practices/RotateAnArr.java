0331cfaa1928da2d9ae25baaca2b0457ef77ebc8
amanraooo <ak9070777@gmail.com>
2025-12-19 23:15:07 +0530

    practise

--- Arrays/basics-and-practices/RotateAnArr.java
+++ Arrays/basics-and-practices/RotateAnArr.java
@@ -1,17 +1,29 @@
 public class RotateAnArr {
     public static void main(String[] args) {
-        int arr[] = {10,20,30,40,50};
-        shiftToRight(arr);
+        int arr[] = { 10, 20, 30, 40, 50 };
+        // shiftToRight(arr);
+        shiftToLeft(arr);
+    }
+
+    public static void shiftToRight(int arr[]) {
+        int temp = arr[arr.length - 1];
+        for (int i = arr.length - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[0] = temp;
+
+        display(arr);
 
     }
-    public static void shiftToRight(int arr[]){
-        int temp = arr[arr.length-1];
-        for (int i = arr.length-1; i >0 ; i--) {
-            arr[i]=arr[i-1];
-        }
-        arr[0]=temp;
 
-   display(arr);
+    public static void shiftToLeft(int arr[]) {
+        int temp = arr[0];
+        for (int i = 0; i < arr.length - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr[arr.length - 1] = temp;
+
+        display(arr);
 
     }
 
@@ -19,5 +31,6 @@ public class RotateAnArr {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
+        System.out.println();
     }
 }
