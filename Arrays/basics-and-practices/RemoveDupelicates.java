0849e1eef0b6d925ef455c49325ebba2f6e5c822
amanraooo <ak9070777@gmail.com>
2025-12-22 20:29:04 +0530

    add course

--- Arrays/basics-and-practices/RemoveDupelicates.java
+++ Arrays/basics-and-practices/RemoveDupelicates.java
@@ -1,26 +1,25 @@
 public class RemoveDupelicates {
     public static void main(String[] args) {
-        int nums[] = {0,0,1,1,1,2,2,3,3,4};
+        int nums[] = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
 
         System.out.println(uniqueElements(nums));
+        int nums1[] = { 1, 1, 2 };
+        System.out.println(uniqueElements(nums1));
 
     }
-    public static int uniqueElements(int arr[]){
+
+    public static int uniqueElements(int arr[]) {
         int unique = 0;
-        
+
         int n = arr.length;
 
         for (int index = 1; index < n; index++) {
-            if(arr[unique]==arr[index]){
-                index++;
-            }
-            else{
+            if (arr[unique] != arr[index]) {
                 unique++;
-                arr[unique]=arr[index];
-                index++;
+                arr[unique] = arr[index];
             }
         }
-        return unique+1 ;
-        
+        return unique + 1;
+
     }
 }
