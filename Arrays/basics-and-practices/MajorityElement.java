b57900507c2a4fdf82062731d736bccb7d3a6fde
amanraooo <ak9070777@gmail.com>
2025-12-24 13:04:28 +0530

    practise

---
+++ Arrays/basics-and-practices/MajorityElement.java
@@ -0,0 +1,27 @@
+public class MajorityElement {
+    public static void main(String[] args) {
+        int nums[] = {2,2,1,1,1,2,2};
+        System.out.println(majorityEle(nums));
+        int nums2[] = {3,2,3};
+        System.out.println(majorityEle(nums2));
+    }
+    public static int majorityEle(int nums[]){
+        int vote_Count = 0;
+        int candidate =0;
+        for (int i = 0; i < nums.length; i++) {
+            int curr_num = nums[i];
+
+            if(vote_Count==0){
+                candidate=curr_num ;
+            }
+            if(curr_num!=candidate){
+                vote_Count--;
+
+            }
+            else{
+                vote_Count++;
+            }
+        }
+        return candidate;
+    }
+}
