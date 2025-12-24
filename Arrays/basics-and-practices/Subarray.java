d2d738d8e10349f13204eb3c7c660c06b80a5843
amanraooo <ak9070777@gmail.com>
2025-12-24 12:27:19 +0530

    practise

--- Arrays/basics-and-practices/Subarray.java
+++ Arrays/basics-and-practices/Subarray.java
@@ -1,7 +1,11 @@
 public class Subarray {
     public static void main(String[] args) {
         int arr[] = { 10, 20, 30, 40 };
-        subArray(arr);
+        int arr1[] = { -2,1,-3,4,-1,2,1,-5,4 };
+
+        // subArray(arr);
+        // subArraySum(arr);
+        System.out.println(MaxSubArraySum(arr1));
     }
 
     public static void subArray(int arr[]) {
@@ -16,5 +20,38 @@ public class Subarray {
             }
 
         }
+    }
+
+    public static void subArraySum(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            int ans = 0;
+            for (int j = i; j < arr.length; j++) {
+                ans = ans + arr[j];
+                System.out.println(ans);
+
+            }
+            System.out.println();
+
+        }
+
+    }
+
+    // using kadance algo: if sum is < 0 sum resets to 0
+    public static int MaxSubArraySum(int arr[]) {
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+            if (sum > max) {
+                max = sum;
+            }
+            if (sum < 0) {
+                sum = 0;
+            }
+
+        }
+        return max;
+
     }
 }
