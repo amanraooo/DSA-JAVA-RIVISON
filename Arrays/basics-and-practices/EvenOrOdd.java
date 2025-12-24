e2aceb6cb73000414d8b3b474bcb029236e8d27c
amanraooo <ak9070777@gmail.com>
2025-12-24 21:27:46 +0530

    practise

--- Arrays/basics-and-practices/EvenOrOdd.java
+++ Arrays/basics-and-practices/EvenOrOdd.java
@@ -2,17 +2,17 @@ public class EvenOrOdd {
     public static void main(String[] args) {
         find(3);
     }
-    public static void find(int num){
-        
-        while(num>2){
-         num= num-2;
+
+    public static void find(int num) {
+
+        while (num > 2) {
+            num = num - 2;
         }
-        if(num==1){
+        if (num == 1) {
             System.out.println("Odd");
-        }
-        else{
+        } else {
             System.out.println("Even");
         }
-        
+
     }
 }
