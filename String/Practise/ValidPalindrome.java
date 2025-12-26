a0b35adbef43d554110e155b5d1e956040310d90
amanraooo <ak9070777@gmail.com>
2025-12-26 21:56:46 +0530

    practise

--- String/Practise/ValidPalindrome.java
+++ String/Practise/ValidPalindrome.java
@@ -4,13 +4,16 @@ public class ValidPalindrome {
         System.out.println(isPalindrome(str));
         String str2 = "naman";
         System.out.println(isPalindrome(str2));
+        String str3 = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(str3));
     }
 
     public static boolean isPalindrome(String str) {
+        str = str.toLowerCase().replaceAll("[^a-z0-9]", "");
         int s = 0;
         int e = str.length() - 1;
 
-        while (s < e) {
+        while (s <= e) {
             if (str.charAt(s) != str.charAt(e)) {
                 return false;
             } else {
