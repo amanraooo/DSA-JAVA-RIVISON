3b14ccb17e1c941f996b25aa75774f41befd88ce
amanraooo <ak9070777@gmail.com>
2025-12-26 23:30:38 +0530

    practise

---
+++ String/Practise/LongestCommonPrefix.java
@@ -0,0 +1,32 @@
+
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+       String strs[] = {"flower","flow","flight"};
+       System.out.println(find(strs));
+
+        String strs2[] = {"dog","racecar","car"};
+       System.out.println(find(strs2));
+
+       String strs3[] = {"aman", "amazing", "amplitude"};
+       System.out.println(find(strs3));
+    }
+    public static String find(String[] strs){
+        if(strs==null || strs.length==0){
+            return "";
+        }
+
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = 0; i < strs[0].length(); i++) {
+           char ch = strs[0].charAt(i);
+
+           for (int j = 1; j < strs.length; j++) {
+            if(strs[j].charAt(i)!=ch || i>=strs.length){
+                return ans.toString();
+            }
+           }
+           ans.append(ch);
+        }
+        return ans.toString();
+    }
+}
