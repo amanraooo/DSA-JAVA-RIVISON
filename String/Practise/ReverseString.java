dd9d02eb98144f0e684802de6c789bf6494465da
amanraooo <ak9070777@gmail.com>
2025-12-26 21:26:44 +0530

    practise

---
+++ String/Practise/ReverseString.java
@@ -0,0 +1,27 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        char s[] = {'h','e','l','l','o'};
+        reverse(s);
+    }
+    public static void reverse(char[] s){
+        int l = 0;
+        int h = s.length-1;
+
+        while (l<=h) {
+            char temp = s[l];
+            s[l] = s[h];
+            s[h] =temp;
+            l++;
+            h--;
+
+        }
+        display(s);
+
+    }
+
+    public static void display(char[] s){
+        for (int index = 0; index < s.length; index++) {
+            System.out.print("'"+ s[index]+ "'" +  ", ");
+        }
+    }
+}
