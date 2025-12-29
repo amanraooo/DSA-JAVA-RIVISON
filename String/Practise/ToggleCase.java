f6d30eb4ac7bcea0c62d6712aa58e0eb29e9661f
amanraooo <ak9070777@gmail.com>
2025-12-29 22:07:58 +0530

    practise

---
+++ String/Practise/ToggleCase.java
@@ -0,0 +1,21 @@
+public class ToggleCase {
+    public static void main(String[] args) {
+         String str = "Aman123@HELlo";
+       
+        System.out.println(togglecase(str));
+    }
+    public static String togglecase(String str){
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if(Character.isUpperCase(ch)){
+               ans.append(Character.toLowerCase(ch));
+            }else if(Character.isLowerCase(ch)){
+               ans.append(Character.toUpperCase(ch));
+            }else {
+                ans.append(ch);
+            }
+        }
+        return ans.toString();
+    }
+}
