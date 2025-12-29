1baf2aedd9cf05b5e30c6cb8c6478d5b1f89da56
amanraooo <ak9070777@gmail.com>
2025-12-29 12:12:24 +0530

    practise

--- String/Practise/ConvertToASCII.java
+++ String/Practise/ConvertToASCII.java
@@ -1,16 +1,15 @@
 public class ConvertToASCII {
     public static void main(String[] args) {
         String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-        //convertString(letters);
+        // convertString(letters);
 
         char[] letters2 = {
-    'A','B','C','D','E','F','G','H','I','J','K','L','M',
-    'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-    'a','b','c','d','e','f','g','h','i','j','k','l','m',
-    'n','o','p','q','r','s','t','u','v','w','x','y','z'
-};
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+        };
         convertChar(letters2);
-
 
     }
 
