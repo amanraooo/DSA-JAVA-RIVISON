b6425db0f647325fa7abfd7b523a3d96277251fc
amanraooo <ak9070777@gmail.com>
2025-12-29 13:03:36 +0530

    practise

---
+++ String/Practise/DiffOfVowelsAndconsonants.java
@@ -0,0 +1,44 @@
+import java.util.Set;
+
+public class DiffOfVowelsAndconsonants {
+    public static void main(String[] args) {
+       
+       String str = "aman";
+       difference(str);
+    }
+    public static void difference(String str){
+         if (str == null || str.isEmpty()) {
+            System.out.println("The string is empty.");
+            System.out.println("Vowels: 0, Consonants: 0, Difference: 0");
+            return;
+        }
+
+        Set<Character> vowelsSet = Set.of('a','e','i','o','u','A','E','I','O','U');
+
+        int vowelsCount=0;
+        int consonantsCount=0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')){
+               if(vowelsSet.contains(ch) ){
+                vowelsCount++;
+                }
+                else{
+                    consonantsCount++;
+                }
+            }
+            else{
+                continue;
+            }
+        }
+        int difference= consonantsCount-vowelsCount;
+
+        System.out.println("Vowels: "+vowelsCount);
+        System.out.println("Consonants: "+consonantsCount);
+        System.out.println("Difference: "+ difference);
+
+    }
+
+    
+}
