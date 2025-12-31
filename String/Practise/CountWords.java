c02874a45613209127c8a292d454ccd1717fcbd0
amanraooo <ak9070777@gmail.com>
2025-12-31 12:58:44 +0530

    practise

--- String/Practise/CountWords.java
+++ String/Practise/CountWords.java
@@ -5,13 +5,13 @@ public class CountWords {
         String s2 = "hello world from java ";
         System.out.println(count(s2));
     }
-    public static int count(String str){
-        int wordCount=0;
-        for (int i = 0; i < str.length(); i++) {
-            
 
-            if((str.charAt(i)!=' ' && (i==0 ||str.charAt(i-1) == ' ')) ){
-wordCount++;
+    public static int count(String str) {
+        int wordCount = 0;
+        for (int i = 0; i < str.length(); i++) {
+
+            if ((str.charAt(i) != ' ' && (i == 0 || str.charAt(i - 1) == ' '))) {
+                wordCount++;
             }
         }
         return wordCount;
