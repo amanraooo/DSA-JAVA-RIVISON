8d990e4512f9f0a18de99e964e40f9e6ddda9237
amanraooo <ak9070777@gmail.com>
2025-12-31 12:39:55 +0530

    practise

--- String/Practise/cleanSpaces.java
+++ String/Practise/cleanSpaces.java
@@ -3,7 +3,8 @@ public class cleanSpaces {
         String s = "   hello   world   from   java   ";
         System.out.println(clean(s));
     }
-    public static String clean(String s){
+
+    public static String clean(String s) {
         s = s.trim();
         StringBuilder result = new StringBuilder();
         boolean space = false;
@@ -11,13 +12,12 @@ public class cleanSpaces {
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
 
-            if(ch == ' '){
-                if(!space){
+            if (ch == ' ') {
+                if (!space) {
                     result.append(ch);
                     space = true;
                 }
-            }
-            else{
+            } else {
                 result.append(ch);
                 space = false;
             }
