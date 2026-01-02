db190ba54d8b1cc0da927b112f0d0f44dfb6a18f
amanraooo <ak9070777@gmail.com>
2026-01-02 22:55:21 +0530

    practise

---
+++ String/Practise/ConsecutiveIdenticalCharacters.java
@@ -0,0 +1,35 @@
+public class ConsecutiveIdenticalCharacters {
+    public static void main(String[] args) {
+       System.out.println(ifConsecutiveIdenticalCharactersExists("aaab"));
+       System.out.println(ifConsecutiveIdenticalCharactersExists("abc"));
+       System.out.println(ifConsecutiveIdenticalCharactersExists("a"));
+       System.out.println(ifConsecutiveIdenticalCharactersExists(""));
+
+
+
+    }
+    public static boolean ifConsecutiveIdenticalCharactersExists(String s){
+        if(s.length()==0||s.length()==1){
+            return false;
+        }
+        int count =1;
+        int i = 1;
+        while (i<s.length()) {
+            if(s.charAt(i)==s.charAt(i-1)){
+                count++;
+                if(count>=2){
+            return true;
+        }
+                i++;
+            }
+            else{
+                count =1;
+                i++; 
+            }
+        }
+
+        
+        
+          return  false;
+            }
+}
