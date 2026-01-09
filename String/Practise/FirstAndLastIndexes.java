686cb150f0e61b8c7714d31d2506d751773c9a08
amanraooo <ak9070777@gmail.com>
2026-01-09 15:56:18 +0530

    practise

--- String/Practise/FirstAndLastIndexes.java
+++ String/Practise/FirstAndLastIndexes.java
@@ -1,17 +1,18 @@
 public class FirstAndLastIndexes {
     public static void main(String[] args) {
-        
+
         find("abcaadb");
         find("");
         find("abcd");
 
     }
-    public static void find(String s){
-        if(s.length()==0){
+
+    public static void find(String s) {
+        if (s.length() == 0) {
             System.out.println("String is empty");
         }
-       int firstOcc[] = new int[256];
-       int LastOcc[] =  new int[256];
+        int firstOcc[] = new int[256];
+        int LastOcc[] = new int[256];
 
         for (int i = 0; i < 256; i++) {
             firstOcc[i] = -1;
@@ -19,15 +20,15 @@ public class FirstAndLastIndexes {
 
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            if(firstOcc[ch]==-1){
+            if (firstOcc[ch] == -1) {
                 firstOcc[ch] = i;
             }
-            LastOcc[ch] =i;
+            LastOcc[ch] = i;
         }
 
         for (int i = 0; i < 256; i++) {
-            if(firstOcc[i]!=-1){
-                System.out.println((char) i + " First: "+ firstOcc[i] + " Last "+ LastOcc[i]);
+            if (firstOcc[i] != -1) {
+                System.out.println((char) i + " First: " + firstOcc[i] + " Last " + LastOcc[i]);
             }
         }
     }
