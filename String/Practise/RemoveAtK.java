e3732c6ffbaf489730700a47885551b502bf8f55
amanraooo <ak9070777@gmail.com>
2026-01-10 19:05:34 +0530

    practise

--- String/Practise/RemoveAtK.java
+++ String/Practise/RemoveAtK.java
@@ -1,35 +1,38 @@
 public class RemoveAtK {
     public static void main(String[] args) {
-        String s ="hello";
+        String s = "hello";
         System.out.println(removeAtFirst(s));
         System.out.println(removeAtLast(s));
-        System.out.println(removeAtK(s,2));
+        System.out.println(removeAtK(s, 2));
 
     }
-    public static String removeAtFirst(String s){
+
+    public static String removeAtFirst(String s) {
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
-            if(i==0){
+            if (i == 0) {
                 continue;
             }
             result.append(s.charAt(i));
         }
         return result.toString();
     }
-    public static String removeAtLast(String s){
+
+    public static String removeAtLast(String s) {
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
-            if(i==s.length()-1){
+            if (i == s.length() - 1) {
                 continue;
             }
             result.append(s.charAt(i));
         }
         return result.toString();
     }
-    public static String removeAtK(String s, int K){
+
+    public static String removeAtK(String s, int K) {
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
-            if(i==K){
+            if (i == K) {
                 continue;
             }
             result.append(s.charAt(i));
