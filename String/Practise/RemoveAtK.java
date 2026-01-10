b76e2713e3dbb2f1cfad374aac05aa6f4eb11e97
amanraooo <ak9070777@gmail.com>
2026-01-10 19:05:09 +0530

    practise

--- String/Practise/RemoveAtK.java
+++ String/Practise/RemoveAtK.java
@@ -3,6 +3,7 @@ public class RemoveAtK {
         String s ="hello";
         System.out.println(removeAtFirst(s));
         System.out.println(removeAtLast(s));
+        System.out.println(removeAtK(s,2));
 
     }
     public static String removeAtFirst(String s){
@@ -19,6 +20,16 @@ public class RemoveAtK {
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
             if(i==s.length()-1){
+                continue;
+            }
+            result.append(s.charAt(i));
+        }
+        return result.toString();
+    }
+    public static String removeAtK(String s, int K){
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if(i==K){
                 continue;
             }
             result.append(s.charAt(i));
