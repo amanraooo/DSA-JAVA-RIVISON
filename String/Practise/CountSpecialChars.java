0a1dd9e49235e711611a455d25ffdffa7c05ded5
amanraooo <ak9070777@gmail.com>
2026-01-10 21:13:38 +0530

    practise

--- String/Practise/CountSpecialChars.java
+++ String/Practise/CountSpecialChars.java
@@ -1,33 +1,33 @@
 public class CountSpecialChars {
     public static void main(String[] args) {
-        //speacial chars are those chars that appears as upper and lowercase both in a string
+        // speacial chars are those chars that appears as upper and lowercase both in a
+        // string
         String word = "aaAbcBC";
         String word1 = "abc";
 
         System.out.println(specialCharacters(word));
         System.out.println(specialCharacters(word1));
 
-
     }
-    public static int specialCharacters(String s){
+
+    public static int specialCharacters(String s) {
         char lower[] = new char[26];
         char upper[] = new char[26];
 
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            if(ch >='a' && ch<='z'){
-                lower[ch-'a']++;
-            }
-            else{
-            upper[ch-'A']++;
+            if (ch >= 'a' && ch <= 'z') {
+                lower[ch - 'a']++;
+            } else {
+                upper[ch - 'A']++;
 
             }
         }
 
-       int count =0;
+        int count = 0;
         for (int i = 0; i < 26; i++) {
-            
-            if(lower[i]!=0 && upper[i]!=0){
+
+            if (lower[i] != 0 && upper[i] != 0) {
                 count++;
             }
 
