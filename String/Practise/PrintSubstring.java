3b288d99f90dd15d565adb1695e1df1056140932
amanraooo <ak9070777@gmail.com>
2026-01-10 19:55:11 +0530

    practise

---
+++ String/Practise/PrintSubstring.java
@@ -0,0 +1,17 @@
+public class PrintSubstring {
+    public static void main(String[] args) {
+        substring("hello");
+    }
+    public static void substring(String s){
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j < s.length(); j++) {
+                for (int p = i; p <=j ; p++) {
+                    System.out.print(s.charAt(p)+ " ");
+                }
+                    System.out.println("---");
+
+            }
+            
+        }
+    }
+}
