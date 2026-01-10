b54ab1c9a1d5f2bfbf7a0512a7671319d3a20e2e
amanraooo <ak9070777@gmail.com>
2026-01-10 20:02:06 +0530

    practise

--- String/Practise/PrintSubstring.java
+++ String/Practise/PrintSubstring.java
@@ -1,8 +1,10 @@
 public class PrintSubstring {
     public static void main(String[] args) {
-        substring("hello");
+        //substring("hello");
+        substring2("hello");
     }
 
+    
     public static void substring(String s) {
         for (int i = 0; i < s.length(); i++) {
             for (int j = i; j < s.length(); j++) {
@@ -12,6 +14,19 @@ public class PrintSubstring {
                 System.out.println("---");
 
             }
+        }
+    }
+    
+    //optimized
+    public static void substring2(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j < s.length(); j++) {
+                
+                    System.out.println(s.substring(i, j+1));
+                
+
+            }
+
         }
     }
 }
