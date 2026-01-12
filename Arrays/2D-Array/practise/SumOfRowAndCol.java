6fcdf19e3bfb13e7f05c9ea71e2ae70700c7df5a
amanraooo <ak9070777@gmail.com>
2026-01-12 13:46:31 +0530

    practise

--- Arrays/2D-Array/practise/SumOfRowAndCol.java
+++ Arrays/2D-Array/practise/SumOfRowAndCol.java
@@ -8,16 +8,17 @@ public class SumOfRowAndCol {
         };
         rowsSum(mat);
     }
-    public static void rowsSum(int mat[][]){
+
+    public static void rowsSum(int mat[][]) {
         for (int i = 0; i < mat.length; i++) {
-        int sum =0;
+            int sum = 0;
 
             for (int j = 0; j < mat[0].length; j++) {
                 sum = sum + mat[i][j];
-                
+
             }
-            System.out.println("row: "+ i);
-                System.out.println("sum "+ sum);
+            System.out.println("row: " + i);
+            System.out.println("sum " + sum);
             System.out.println();
         }
     }
