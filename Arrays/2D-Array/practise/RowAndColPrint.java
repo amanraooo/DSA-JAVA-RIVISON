40bf08683eb2a74e684c09b3f0e763483f73e47b
amanraooo <ak9070777@gmail.com>
2026-01-12 13:25:10 +0530

    practise

--- Arrays/2D-Array/practise/RowAndColPrint.java
+++ Arrays/2D-Array/practise/RowAndColPrint.java
@@ -7,12 +7,22 @@ public class RowAndColPrint {
         };
 
         rowPrint(mat);
+        System.out.println("----");
+        colPrint(mat);
     }
 
     public static void rowPrint(int mat[][]) {
         for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat[0].length; j++) {
                 System.out.print(mat[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void colPrint(int mat[][]) {
+        for (int i = 0; i < mat[0].length; i++) {
+            for (int j = 0; j < mat.length; j++) {
+                System.out.print(mat[j][i] + " ");
             }
             System.out.println();
         }
