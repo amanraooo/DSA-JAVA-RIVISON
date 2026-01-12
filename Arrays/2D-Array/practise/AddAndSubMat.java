ec0139eefdd153e81107b63fbd9e83d0769a93d5
amanraooo <ak9070777@gmail.com>
2026-01-12 15:44:26 +0530

    practise

--- Arrays/2D-Array/practise/AddAndSubMat.java
+++ Arrays/2D-Array/practise/AddAndSubMat.java
@@ -1,25 +1,36 @@
 
 public class AddAndSubMat {
     public static void main(String[] args) {
-         int[][] mat1 = {
-                {1, 2, 3},
-                {4, 5, 6}
+        int[][] mat1 = {
+                { 1, 2, 3 },
+                { 4, 5, 6 }
         };
 
         int[][] mat2 = {
-                {7, 8, 9},
-                {1, 2, 3}
+                { 7, 8, 9 },
+                { 10, 11, 12 }
         };
 
-        int res[][] = addMats(mat1, mat2);
-        for (int i = 0; i < res.length; i++) {
-            for (int j = 0; j < res[0].length; j++) {
-                System.out.print(res[i][j] + " ");
+        int addRes[][] = addMats(mat1, mat2);
+        for (int i = 0; i < addRes.length; i++) {
+            for (int j = 0; j < addRes[0].length; j++) {
+                System.out.print(addRes[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("----");
+
+        int subRes[][] = subMats(mat1, mat2);
+        for (int i = 0; i < subRes.length; i++) {
+            for (int j = 0; j < subRes[0].length; j++) {
+                System.out.print(subRes[i][j] + " ");
             }
             System.out.println();
         }
     }
-    public static int[][] addMats(int mat1[][], int mat2[][]){
+
+    public static int[][] addMats(int mat1[][], int mat2[][]) {
         int result[][] = new int[mat1.length][mat1[0].length];
 
         for (int i = 0; i < result.length; i++) {
@@ -30,5 +41,15 @@ public class AddAndSubMat {
         return result;
     }
 
+    public static int[][] subMats(int mat1[][], int mat2[][]) {
+        int result[][] = new int[mat1.length][mat1[0].length];
+
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[0].length; j++) {
+                result[i][j] = mat2[i][j] - mat1[i][j];
+            }
+        }
+        return result;
+    }
 
 }
