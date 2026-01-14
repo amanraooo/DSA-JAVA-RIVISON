cd8acf032791499b3ff038707bbb7149b563e1b0
amanraooo <ak9070777@gmail.com>
2026-01-14 16:56:17 +0530

    practise

--- Arrays/2D-Array/practise/SymmetricMat.java
+++ Arrays/2D-Array/practise/SymmetricMat.java
@@ -1,25 +1,26 @@
 public class SymmetricMat {
     public static void main(String[] args) {
         int[][] mat1 = {
-                {  6 ,5, 2},
+                { 6, 5, 2 },
                 { 5, 0, 9 },
-                {2, 9, 3}
+                { 2, 9, 3 }
         };
         int[][] mat2 = {
-                {  6 ,1 ,2},
+                { 6, 1, 2 },
                 { 5, 0, 9 },
-                {2, 9, 3}
+                { 2, 9, 3 }
         };
-       System.out.println(isSymmetric(mat1)); 
-       System.out.println(isSymmetric(mat2)); 
+        System.out.println(isSymmetric(mat1));
+        System.out.println(isSymmetric(mat2));
 
     }
-    public static boolean isSymmetric(int mat[][]){
+
+    public static boolean isSymmetric(int mat[][]) {
         int transposeMat[][] = transpose(mat);
 
         for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat[0].length; j++) {
-                if(mat[i][j]!=transposeMat[i][j]){
+                if (mat[i][j] != transposeMat[i][j]) {
                     return false;
                 }
             }
@@ -27,12 +28,13 @@ public class SymmetricMat {
         return true;
 
     }
-    public static int [][]transpose(int mat[][]){
+
+    public static int[][] transpose(int mat[][]) {
         int result[][] = new int[mat.length][mat[0].length];
-        
+
         for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat[0].length; j++) {
-                result[i][j] = mat[j][i]; 
+                result[i][j] = mat[j][i];
             }
         }
         return result;
