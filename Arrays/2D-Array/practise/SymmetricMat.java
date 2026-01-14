6e611026a247b0aebdccb7e3ca763dff0bd057aa
amanraooo <ak9070777@gmail.com>
2026-01-14 16:22:12 +0530

    practise

---
+++ Arrays/2D-Array/practise/SymmetricMat.java
@@ -0,0 +1,40 @@
+public class SymmetricMat {
+    public static void main(String[] args) {
+        int[][] mat1 = {
+                {  6 ,5, 2},
+                { 5, 0, 9 },
+                {2, 9, 3}
+        };
+        int[][] mat2 = {
+                {  6 ,1 ,2},
+                { 5, 0, 9 },
+                {2, 9, 3}
+        };
+       System.out.println(isSymmetric(mat1)); 
+       System.out.println(isSymmetric(mat2)); 
+
+    }
+    public static boolean isSymmetric(int mat[][]){
+        int transposeMat[][] = transpose(mat);
+
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if(mat[i][j]!=transposeMat[i][j]){
+                    return false;
+                }
+            }
+        }
+        return true;
+
+    }
+    public static int [][]transpose(int mat[][]){
+        int result[][] = new int[mat.length][mat[0].length];
+        
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                result[i][j] = mat[j][i]; 
+            }
+        }
+        return result;
+    }
+}
