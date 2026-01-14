63619af0ad5b5672d5726ed882c7e838cd4ac348
amanraooo <ak9070777@gmail.com>
2026-01-14 16:02:54 +0530

    practise

--- Arrays/2D-Array/practise/PrintZigZagMat.java
+++ Arrays/2D-Array/practise/PrintZigZagMat.java
@@ -1,6 +1,6 @@
 public class PrintZigZagMat {
     public static void main(String[] args) {
-         int mat1[][] = {
+        int mat1[][] = {
                 { 1, 2, 3 },
                 { 4, 5, 6 },
                 { 7, 8, 9 },
@@ -24,16 +24,16 @@ public class PrintZigZagMat {
         System.out.println("-----");
         zigzag(mat3);
     }
-    public static void zigzag(int mat[][]){
+
+    public static void zigzag(int mat[][]) {
         for (int i = 0; i < mat.length; i++) {
-            if(i%2==0){
+            if (i % 2 == 0) {
                 for (int j = 0; j < mat[0].length; j++) {
-                    System.out.print(mat[i][j]+" ");
+                    System.out.print(mat[i][j] + " ");
                 }
-            }
-            else{
-                for (int j = mat[0].length-1; j >= 0; j--) {
-                    System.out.print(mat[i][j]+" ");
+            } else {
+                for (int j = mat[0].length - 1; j >= 0; j--) {
+                    System.out.print(mat[i][j] + " ");
                 }
             }
             System.out.println();
