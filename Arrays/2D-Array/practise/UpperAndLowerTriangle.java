375d30da31cb615c60a6b627f5f0567dd0b6f7df
amanraooo <ak9070777@gmail.com>
2026-01-14 14:15:31 +0530

    practise

--- Arrays/2D-Array/practise/UpperAndLowerTriangle.java
+++ Arrays/2D-Array/practise/UpperAndLowerTriangle.java
@@ -6,12 +6,27 @@ public class UpperAndLowerTriangle {
                 { 7, 8, 9 }
         };
         upperTriangle(mat);
+        System.out.println("----");
+        lowerTriangle(mat);
     }
 
     public static void upperTriangle(int mat[][]) {
         for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat[0].length; j++) {
                 if (j >= i) {
+                    System.out.print(mat[i][j] + " ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void lowerTriangle(int mat[][]) {
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if (i >= j) {
                     System.out.print(mat[i][j] + " ");
                 } else {
                     System.out.print("  ");
