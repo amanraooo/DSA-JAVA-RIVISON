a1243a4d5ca608a3c40aa417efcc0640e4aaec99
amanraooo <ak9070777@gmail.com>
2026-01-15 23:35:58 +0530

    practise

--- Arrays/2D-Array/practise/SortMatrix.java
+++ Arrays/2D-Array/practise/SortMatrix.java
@@ -2,37 +2,39 @@ import java.util.Arrays;
 
 public class SortMatrix {
     public static void main(String[] args) {
-        int mat[][]={
-            {9,3,5},
-            {4,1,8},
-            {6,2,7}
+        int mat[][] = {
+                { 9, 3, 5 },
+                { 4, 1, 8 },
+                { 6, 2, 7 }
         };
         sort(mat);
+        display(mat);
     }
-    public static void sort(int mat[][]){
+
+    public static void sort(int mat[][]) {
         int n = mat.length;
         int m = mat[0].length;
 
-        int arr[] = new int[n*m];
-        int k =0;
+        int arr[] = new int[n * m];
+        int k = 0;
         for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat[0].length; j++) {
-                arr[k++]= mat[i][j];
+                arr[k++] = mat[i][j];
             }
         }
 
         Arrays.sort(arr);
-        
-        k =0;
+
+        k = 0;
         for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat[0].length; j++) {
-                 mat[i][j]=arr[k++];
+                mat[i][j] = arr[k++];
             }
         }
 
     }
 
-    public static void display(int mat[][]){
+    public static void display(int mat[][]) {
         for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat[0].length; j++) {
                 System.out.print(mat[i][j] + " ");
