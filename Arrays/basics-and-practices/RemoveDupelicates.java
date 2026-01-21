f7a5285a39f273139ff4c4d10ce376d391250df8
amanraooo <ak9070777@gmail.com>
2026-01-21 14:00:40 +0530

    practise

--- Arrays/basics-and-practices/RemoveDupelicates.java
+++ Arrays/basics-and-practices/RemoveDupelicates.java
@@ -12,36 +12,37 @@ public class RemoveDupelicates {
         uniqueElements2(nums3);
         uniqueElements2(nums4);
     }
-    //return count of unique elements
-    public static int uniqueElements(int arr[]) {
-       int unique =0;
-       int n = arr.length;
 
-       for (int i = 1; i <n; i++) {
-        if(arr[unique]!=arr[i]){
-            unique++;
-            arr[unique]=arr[i];
+    // return count of unique elements
+    public static int uniqueElements(int arr[]) {
+        int unique = 0;
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            if (arr[unique] != arr[i]) {
+                unique++;
+                arr[unique] = arr[i];
+            }
         }
-       }
-       return unique+1;
+        return unique + 1;
 
     }
 
-    //print those unique elements
+    // print those unique elements
     public static void uniqueElements2(int arr[]) {
-       int unique =0;
-       int n = arr.length;
+        int unique = 0;
+        int n = arr.length;
 
-       for (int i = 1; i < n; i++) {
-        if(arr[unique]!=arr[i]){
-            unique++;
-            arr[unique]=arr[i];
+        for (int i = 1; i < n; i++) {
+            if (arr[unique] != arr[i]) {
+                unique++;
+                arr[unique] = arr[i];
+            }
         }
-       }
-       for (int i = 0; i < unique+1; i++) {
-        System.out.print(arr[i]+" ");
-       }
-       System.out.println();
+        for (int i = 0; i < unique + 1; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
 
     }
 }
