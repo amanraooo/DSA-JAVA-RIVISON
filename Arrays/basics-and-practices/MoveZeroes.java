3409627ad551ecb5d924d44ca9435220ccb32fa2
amanraooo <ak9070777@gmail.com>
2026-01-21 13:32:42 +0530

    practise

--- Arrays/basics-and-practices/MoveZeroes.java
+++ Arrays/basics-and-practices/MoveZeroes.java
@@ -7,16 +7,16 @@ public class MoveZeroes {
     }
 
     public static void move(int nums[]) {
-        int pos =0;
+        int pos = 0;
         for (int i = 0; i < nums.length; i++) {
-            if(nums[i]!=0){
-                nums[pos]=nums[i];
+            if (nums[i] != 0) {
+                nums[pos] = nums[i];
                 pos++;
             }
         }
 
-        while (pos<nums.length) {
-            nums[pos]=0;
+        while (pos < nums.length) {
+            nums[pos] = 0;
             pos++;
         }
         display(nums);
