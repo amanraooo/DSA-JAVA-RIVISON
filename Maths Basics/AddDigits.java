86c233ce101a974c91659a03e247e6046819f5ea
amanraooo <ak9070777@gmail.com>
2026-02-02 12:25:47 +0530

    sum of digits

--- Maths Basics/AddDigits.java
+++ Maths Basics/AddDigits.java
@@ -4,15 +4,15 @@ public class AddDigits {
         System.out.println(add(1024));
         System.out.println(add(9817));
 
-
     }
-    public static int add(int n ){
-        int sum =0;
-        while(n>0){
+
+    public static int add(int n) {
+        int sum = 0;
+        while (n > 0) {
             int lastDigit = n % 10;
             sum = sum + lastDigit;
             n = n / 10;
         }
-        return sum ;
+        return sum;
     }
 }
