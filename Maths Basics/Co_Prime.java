5a488dcb96afb5737c6fce317a00482058bed18d
amanraooo <ak9070777@gmail.com>
2026-02-02 17:22:32 +0530

    co-prime

--- Maths Basics/Co_Prime.java
+++ Maths Basics/Co_Prime.java
@@ -5,16 +5,19 @@ public class Co_Prime {
         System.out.println(isCoprime(12, 18));
 
     }
-    public static boolean isCoprime(int a, int b ){
-        int hcf =findHCF(a, b);
-        return hcf==1;
+
+    public static boolean isCoprime(int a, int b) {
+        int hcf = findHCF(a, b);
+        return hcf == 1;
     }
-    public static int findHCF(int a,int b){
-        int min = Math.min(a,b);
-        int hcf =0;
+
+    public static int findHCF(int a, int b) {
+        int min = Math.min(a, b);
+        int hcf = 0;
+        
         for (int index = 1; index <= min; index++) {
-            if(a%index==0 && b%index==0){
-                hcf =index;
+            if (a % index == 0 && b % index == 0) {
+                hcf = index;
             }
         }
         return hcf;
