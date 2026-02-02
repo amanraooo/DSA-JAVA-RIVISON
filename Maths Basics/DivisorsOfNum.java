131d195869b7dc722e24c610a0ac33699a190942
amanraooo <ak9070777@gmail.com>
2026-02-02 13:54:26 +0530

    divisors

---
+++ Maths Basics/DivisorsOfNum.java
@@ -0,0 +1,16 @@
+
+public class DivisorsOfNum {
+    public static void main(String[] args) {
+        divisor(12);
+        divisor(36);
+
+    }
+    public static void divisor(int n){
+        for (int i = 1; i <=n; i++) {
+            if(n%i==0){
+                System.out.print(i+ " ");
+            }
+        }
+        System.out.println();
+    }
+}
