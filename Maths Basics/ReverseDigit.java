ac65adeffb79cecfe5e0671a3bbf47342b923417
amanraooo <ak9070777@gmail.com>
2026-02-02 12:45:30 +0530

    sum of digits

--- Maths Basics/ReverseDigit.java
+++ Maths Basics/ReverseDigit.java
@@ -3,11 +3,12 @@ public class ReverseDigit {
     public static void main(String[] args) {
         System.out.println(reverse(9817));
     }
-    public static int reverse(int n ){
-        int reversed =0;
-        while(n>0){
+
+    public static int reverse(int n) {
+        int reversed = 0;
+        while (n > 0) {
             int lastDigit = n % 10;
-            reversed =    reversed* 10 + lastDigit;
+            reversed = reversed * 10 + lastDigit;
             n = n / 10;
         }
         return reversed;
