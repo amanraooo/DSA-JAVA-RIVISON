c63feef61bf61a3019e67ad6942edaa36d1804ac
amanraooo <ak9070777@gmail.com>
2026-02-02 16:25:31 +0530

    armstrong

--- Maths Basics/ArmstrongNumber.java
+++ Maths Basics/ArmstrongNumber.java
@@ -4,36 +4,39 @@ public class ArmstrongNumber {
         System.out.println(isArmstrong(1634));
         System.out.println(isArmstrong(533));
 
-
     }
-    public static boolean isArmstrong(int n ){
-        int og =n;
+
+    public static boolean isArmstrong(int n) {
+        int og = n;
         int power = countdigits(n);
+        int sum = 0;
 
-        int sum =0;
-        while(n>0){
-        int digitPower =1;
-
+        while (n > 0) {
+            int digitPower = 1;
             int lastDigit = n % 10;
-      for (int index = 1; index <= power; index++) {
-            digitPower = digitPower*lastDigit;
+
+            for (int index = 1; index <= power; index++) {
+                digitPower = digitPower * lastDigit;
+            }
+
+            sum = sum + digitPower;
+            n = n / 10;
+
         }
-        sum = sum + digitPower;
-        n = n /10;
         
-        }
-        if(sum==og){
+        if (sum == og) {
             return true;
         }
         return false;
-        
+
     }
-    public static int countdigits(int n ){
-        int count=0;
-        while(n>0){
+
+    public static int countdigits(int n) {
+        int count = 0;
+        while (n > 0) {
             int lastDigit = n % 10;
-             count++;
-            n = n/10;
+            count++;
+            n = n / 10;
         }
         return count;
     }
