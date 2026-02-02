9f6cf5959cdbc0154368fe21c3fe2345bb8ee2cf
amanraooo <ak9070777@gmail.com>
2026-02-02 12:46:48 +0530

    reverse digits

--- Maths Basics/ReverseDigit.java
+++ Maths Basics/ReverseDigit.java
@@ -1,6 +1,8 @@
 
 public class ReverseDigit {
     public static void main(String[] args) {
+        System.out.println(reverse(5382));
+        System.out.println(reverse(1024));
         System.out.println(reverse(9817));
     }
 
