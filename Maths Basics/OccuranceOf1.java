55562d55fe08121d8bfd89f15e3df0b551349e3b
amanraooo <ak9070777@gmail.com>
2026-02-02 16:41:32 +0530

    armstrong

--- Maths Basics/OccuranceOf1.java
+++ Maths Basics/OccuranceOf1.java
@@ -5,16 +5,20 @@ public class OccuranceOf1 {
         System.out.println(find(100));
 
     }
-    public static int find(int n ){
-        int count=0;
+
+    public static int find(int n) {
+        int count = 0;
+
         for (int i = 1; i <= n; i++) {
             int num = i;
-            while(num>0){
-                int lastdigit = num%10;
-                if(lastdigit==1){
+
+            while (num > 0) {
+                int lastdigit = num % 10;
+                if (lastdigit == 1) {
                     count++;
                 }
-                num = num/10;
+                
+                num = num / 10;
             }
         }
         return count;
