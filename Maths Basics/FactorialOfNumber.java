7a7f97fb058787cc2e0b3f24756f037458f4a3f7
amanraooo <ak9070777@gmail.com>
2026-02-02 13:07:39 +0530

    factorial

---
+++ Maths Basics/FactorialOfNumber.java
@@ -0,0 +1,15 @@
+
+public class FactorialOfNumber {
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+        System.out.println(factorial(4));
+
+    }
+    public static int factorial(int n){
+        int fact = 1;
+        for(int i= n ;i>0; i--  ){
+            fact = fact *i;
+        }
+        return fact;
+    }
+}
