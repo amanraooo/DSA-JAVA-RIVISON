b0bfca9eab9546433d7835130d628886a021f66a
amanraooo <ak9070777@gmail.com>
2026-02-04 19:02:11 +0530

    fibbonacci series

--- Maths Basics/Fibonacci.java
+++ Maths Basics/Fibonacci.java
@@ -6,20 +6,20 @@ public class Fibonacci {
         fibbonacciseries(20);
         fibbonacciseries(25);
 
-
     }
-    public static void fibbonacciseries(int n){
+
+    public static void fibbonacciseries(int n) {
         int a = 0;
-        int b =1;
-        System.out.print(a+ " "+b +" ");
+        int b = 1;
+        System.out.print(a + " " + b + " ");
 
         while (true) {
-            int next = a +b;
-            if(next>n){
+            int next = a + b;
+            if (next > n) {
                 break;
             }
-            System.out.print(next+" ");
-            a = b ;
+            System.out.print(next + " ");
+            a = b;
             b = next;
         }
         System.out.println();
