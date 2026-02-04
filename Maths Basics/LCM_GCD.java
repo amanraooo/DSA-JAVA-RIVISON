4c5cb0462757de222b291201d99619dd43fad0f5
amanraooo <ak9070777@gmail.com>
2026-02-04 18:23:29 +0530

    lcm gcd

---
+++ Maths Basics/LCM_GCD.java
@@ -0,0 +1,22 @@
+public class LCM_GCD {
+    public static void main(String[] args) {
+        int a = 12;
+        int b =18;
+        int gcd = Findgcd(a,b);
+        int lcm = (a*b)/gcd;
+       System.out.println("GCD "+ gcd);
+       System.out.println("LCM "+ lcm);
+        
+    }
+    public static int Findgcd(int a, int b){
+        int gcd=1;
+        int min = Math.min(a,b);
+
+        for(int i = 1; i<=min; i++){
+            if(a%i==0 && b %i==0){
+                gcd = i;
+            }
+        }
+        return gcd;
+    }
+}
