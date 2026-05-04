9e2557adac554a1cdd4183ff11c056c855310217
Aman Gaade <ak9070777@gmail.com>
2026-05-04 14:53:46 +0530

    merge 2 2d arrays

--- 2Pointers/Merge_2_2D_Arrays_by_summing_values.java
+++ 2Pointers/Merge_2_2D_Arrays_by_summing_values.java
@@ -2,74 +2,70 @@ public class Merge_2_2D_Arrays_by_summing_values {
 
     public static int[][] mergeArrays(int[][] nums1, int[][] nums2) {
 
-        int hash[] = new int [1001];
+        int hash[] = new int[1001];
 
-        for(int i = 0; i<nums1.length; i++){
+        for (int i = 0; i < nums1.length; i++) {
             hash[nums1[i][0]]++;
         }
-        for(int i = 0; i<nums2.length; i++){
+        for (int i = 0; i < nums2.length; i++) {
             hash[nums2[i][0]]++;
         }
 
-        int count=0;
+        int count = 0;
         for (int i = 0; i < hash.length; i++) {
-            if(hash[i]!=0){
+            if (hash[i] != 0) {
                 count++;
             }
         }
 
-        int result [][]=new int[count][2];
+        int result[][] = new int[count][2];
 
-        int i =0;
-        int j =0;
-        int r =0;
+        int i = 0;
+        int j = 0;
+        int r = 0;
 
-        while (i<nums1.length&& j<nums2.length) {
-            int id1= nums1[i][0];
-            int id2= nums2[j][0];
+        while (i < nums1.length && j < nums2.length) {
+            int id1 = nums1[i][0];
+            int id2 = nums2[j][0];
 
-            int val1 =nums1[i][1];
-            int val2 =nums2[j][1];
+            int val1 = nums1[i][1];
+            int val2 = nums2[j][1];
 
-            if(id1==id2){
-                result[r][0]= id1;
-                result[r][1]= val1+val2;
+            if (id1 == id2) {
+                result[r][0] = id1;
+                result[r][1] = val1 + val2;
                 i++;
                 j++;
                 r++;
-            }
-            else if(id1<id2){
-                result[r][0]=id1;
-                result[r][1]=val1;
+            } else if (id1 < id2) {
+                result[r][0] = id1;
+                result[r][1] = val1;
                 i++;
                 r++;
-            }
-            else{
-                result[r][0]=id2;
-                result[r][1]=val2;
+            } else {
+                result[r][0] = id2;
+                result[r][1] = val2;
                 j++;
                 r++;
             }
         }
-            while (i<nums1.length) {
-                result[r][0]= nums1[i][0];
-                result[r][1]=nums1[i][1];
-                i++;
-                r++;
-            }
-
-            while (j<nums2.length) {
-                result[r][0]= nums2[j][0];
-                result[r][1]=nums2[j][1];
-                j++;
-                r++;
-            }
-
-            return result;
-
+        while (i < nums1.length) {
+            result[r][0] = nums1[i][0];
+            result[r][1] = nums1[i][1];
+            i++;
+            r++;
         }
-        
-    
+
+        while (j < nums2.length) {
+            result[r][0] = nums2[j][0];
+            result[r][1] = nums2[j][1];
+            j++;
+            r++;
+        }
+
+        return result;
+
+    }
 
     public static void printArr(int matrix[][]) {
         for (int[] row : matrix) {
