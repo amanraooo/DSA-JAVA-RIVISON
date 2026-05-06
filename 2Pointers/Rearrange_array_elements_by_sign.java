d3e1eeb1c5d5ebb715398af14e255c535ba8bbb5
Aman Gaade <ak9070777@gmail.com>
2026-05-06 16:51:40 +0530

    Rearrange_array_elements_by_sign

--- 2Pointers/Rearrange_array_elements_by_sign.java
+++ 2Pointers/Rearrange_array_elements_by_sign.java
@@ -1,34 +1,34 @@
 public class Rearrange_array_elements_by_sign {
     public static void main(String[] args) {
-        int []nums = {3,1,-2,-5,2,-4};
+        int[] nums = { 3, 1, -2, -5, 2, -4 };
         display(rearrange(nums));
 
-        int []nums1 = {-1,1};
+        int[] nums1 = { -1, 1 };
         display(rearrange(nums1));
 
-        int []nums2 = {1,2,3,4,-1,-2,-3,-4};
+        int[] nums2 = { 1, 2, 3, 4, -1, -2, -3, -4 };
         display(rearrange(nums2));
 
-        int []nums3 = {28,-41,22,-8,-37,46,35,-9,18,-6,19,-26,-37,-10,-9,15,14,31};
+        int[] nums3 = { 28, -41, 22, -8, -37, 46, 35, -9, 18, -6, 19, -26, -37, -10, -9, 15, 14, 31 };
         display(rearrange(nums3));
     }
-    public static int [] rearrange(int arr[]){
-         int pos =0;
+
+    public static int[] rearrange(int arr[]) {
+        int pos = 0;
         int neg = 1;
-        
-        int ans[]= new int[arr.length];
+
+        int ans[] = new int[arr.length];
 
         for (int i = 0; i < arr.length; i++) {
-            if(arr[i]>0){
-                ans[pos]= arr[i];
-                pos+=2;
-            }
-            else{
-                ans[neg]=arr[i];
-                neg+=2;
+            if (arr[i] > 0) {
+                ans[pos] = arr[i];
+                pos += 2;
+            } else {
+                ans[neg] = arr[i];
+                neg += 2;
             }
         }
-        
+
         return ans;
     }
 
