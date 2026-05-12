544faa6ec2f4c278cde43ff366a1170d6a46f13b
Aman Gaade <ak9070777@gmail.com>
2026-05-12 18:09:33 +0530

    Remove_Duplicates_from_sorted_array

--- 2Pointers/Remove_Duplicates_from_sorted_array.java
+++ 2Pointers/Remove_Duplicates_from_sorted_array.java
@@ -1,21 +1,21 @@
 public class Remove_Duplicates_from_sorted_array {
     public static void main(String[] args) {
-        int nums[] = {0,0,1,1,1,2,2,3,3,4};
+        int nums[] = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
         System.out.println(remove(nums));
 
     }
 
-    public static int remove(int arr[]){
+    public static int remove(int arr[]) {
 
-        int u=0;
-        int i=1;
+        int u = 0;
+        int i = 1;
 
         for (int index = 0; index < arr.length; index++) {
-            if(arr[u]!=arr[index]){
+            if (arr[u] != arr[index]) {
                 u++;
-                arr[u]=arr[index];
+                arr[u] = arr[index];
             }
         }
-        return u+1;
+        return u + 1;
     }
 }
