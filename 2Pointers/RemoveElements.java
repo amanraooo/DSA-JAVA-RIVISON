a528fedd67c32b3352ee52682d44069bd42ff944
Aman Gaade <ak9070777@gmail.com>
2026-05-12 18:38:25 +0530

    remove elements

--- 2Pointers/RemoveElements.java
+++ 2Pointers/RemoveElements.java
@@ -1,13 +1,14 @@
 public class RemoveElements {
     public static void main(String[] args) {
-        int nums[]={0,1,2,2,3,0,4,2};
+        int nums[] = { 0, 1, 2, 2, 3, 0, 4, 2 };
         System.out.println(removeEle(nums, 2));
     }
-    public static int removeEle(int nums[], int val){
-        int r=0;
+
+    public static int removeEle(int nums[], int val) {
+        int r = 0;
         for (int i = 0; i < nums.length; i++) {
-            if(nums[i]!=val){
-                nums[r]=nums[i];
+            if (nums[i] != val) {
+                nums[r] = nums[i];
                 r++;
             }
         }
