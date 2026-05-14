1646a5c3b14795057e05dc64a7dcabae1e4bc9a5
Aman Gaade <ak9070777@gmail.com>
2026-05-14 16:23:02 +0530

    rotate array

--- 2Pointers/Rotate_Array.java
+++ 2Pointers/Rotate_Array.java
@@ -1,30 +1,31 @@
 public class Rotate_Array {
     public static void main(String[] args) {
         int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
-        //rotateBruteForce(arr, 3);
-        rotateOptimized(arr,3);
+        // rotateBruteForce(arr, 3);
+        rotateOptimized(arr, 3);
     }
 
-    public static void rotateOptimized(int arr[], int k){
-        
-        reverseArray(arr,0, arr.length-1);
-    reverseArray(arr,0,k-1);
-    reverseArray(arr,k,arr.length-1);
+    public static void rotateOptimized(int arr[], int k) {
 
-    display(arr);
+        reverseArray(arr, 0, arr.length - 1);
+        reverseArray(arr, 0, k - 1);
+        reverseArray(arr, k, arr.length - 1);
+
+        display(arr);
 
     }
 
-    public static void reverseArray(int arr[], int s , int e){
-        
-        while(s<=e){
+    public static void reverseArray(int arr[], int s, int e) {
+
+        while (s <= e) {
             int temp = arr[s];
-            arr[s]=arr[e];
-            arr[e]=temp;
+            arr[s] = arr[e];
+            arr[e] = temp;
             s++;
             e--;
         }
     }
+
     // brute force
     public static void rotateBruteForce(int arr[], int k) {
 
