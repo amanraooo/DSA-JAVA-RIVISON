5abf3af551ef7e4cc0fca64671ac314e00ecdddf
Aman Gaade <ak9070777@gmail.com>
2026-05-19 17:59:09 +0530

    sort colors

--- 2Pointers/Sort_colors.java
+++ 2Pointers/Sort_colors.java
@@ -1,42 +1,42 @@
 public class Sort_colors {
     public static void main(String[] args) {
-        int nums[] = {2,0,2,1,1,0};
+        int nums[] = { 2, 0, 2, 1, 1, 0 };
         sortColors(nums);
     }
-    public static void sortColors(int colors[]){
 
-        int zeroes=0;
-        int ones=0;
-        int twos=0;
+    public static void sortColors(int colors[]) {
+
+        int zeroes = 0;
+        int ones = 0;
+        int twos = 0;
 
         for (int index = 0; index < colors.length; index++) {
-            if(colors[index]==0){
+            if (colors[index] == 0) {
                 zeroes++;
-            }
-            else if(colors[index]==1){
+            } else if (colors[index] == 1) {
                 ones++;
-            }
-            else{
+            } else {
                 twos++;
             }
         }
 
-        int i =0;
-        while(zeroes --> 0){
-            colors[i]=0;
+        int i = 0;
+        while (zeroes-- > 0) {
+            colors[i] = 0;
             i++;
         }
-        while(ones --> 0){
-            colors[i]=1;
+        while (ones-- > 0) {
+            colors[i] = 1;
             i++;
         }
-        while(twos --> 0){
-            colors[i]=2;
+        while (twos-- > 0) {
+            colors[i] = 2;
             i++;
         }
 
         display(colors);
     }
+
     public static void display(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
