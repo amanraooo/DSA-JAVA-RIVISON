1df9816aa2ec0cae8acc48a3dac6d3af20b26ed3
Aman Gaade <ak9070777@gmail.com>
2026-06-03 17:49:07 +0530

    Three_sum_closest

--- 2Pointers/Three_sum_closest.java
+++ 2Pointers/Three_sum_closest.java
@@ -2,9 +2,11 @@ import java.util.Arrays;
 
 public class Three_sum_closest {
     public static void main(String[] args) {
-        int nums[]={-1,2,1,-4}; int target =1;
-        System.out.println(threeSumClosest(nums,target));
+        int nums[] = { -1, 2, 1, -4 };
+        int target = 1;
+        System.out.println(threeSumClosest(nums, target));
     }
+
     public static int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
 
@@ -38,5 +40,5 @@ public class Three_sum_closest {
         }
 
         return resSum;
-    } 
+    }
 }
