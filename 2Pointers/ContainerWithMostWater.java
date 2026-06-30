42977df201f58def4648c2247df04ca9a9a32c6a
Aman Gaade <ak9070777@gmail.com>
2026-06-30 17:40:13 +0530

    container with most water

--- 2Pointers/ContainerWithMostWater.java
+++ 2Pointers/ContainerWithMostWater.java
@@ -1,24 +1,28 @@
-public class ContainerWithMostWater{
+public class ContainerWithMostWater {
     public static void main(String[] args) {
-       int height[] = {1,8,6,2,5,4,8,3,7};
+        int height[] = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
 
-       System.out.println(maxArea(height));
+        System.out.println(maxArea(height));
     }
-    public static int maxArea(int arr[]){
-        int l=0;
-        int r = arr.length-1;
-        int max =0;
 
-        while (l<r) {
+    public static int maxArea(int arr[]) {
+        int l = 0;
+        int r = arr.length - 1;
+        int max = 0;
+
+        while (l < r) {
             int w = r - l;
             int h = Math.min(arr[l], arr[r]);
-            int area = w*h;
+            int area = w * h;
             max = Math.max(max, area);
 
-            if(arr[l]<arr[r])l++;
-            else if(arr[l]>arr[r]) r--;
-            else{
-                l++; r--;
+            if (arr[l] < arr[r])
+                l++;
+            else if (arr[l] > arr[r])
+                r--;
+            else {
+                l++;
+                r--;
             }
         }
         return max;
