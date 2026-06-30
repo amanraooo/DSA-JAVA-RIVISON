40b84a0f7f458f24c96efc8fc361655c64d92519
Aman Gaade <ak9070777@gmail.com>
2026-06-30 18:02:00 +0530

    check if sorted and rotated

--- 2Pointers/Check_If_arr_sortedAnd_rotated.java
+++ 2Pointers/Check_If_arr_sortedAnd_rotated.java
@@ -1,7 +1,10 @@
 public class Check_If_arr_sortedAnd_rotated {
     public static void main(String[] args) {
         int nums[] = { 3, 4, 5, 1, 2 };
+        int nums1[] = {2,1,3,4};
         System.out.println(check(nums));
+        System.out.println(check(nums1));
+
     }
 
     public static boolean check(int[] nums) {
