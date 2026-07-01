2d5e0692934cbffde3aff3a12290e108dfc4a50d
Aman Gaade <ak9070777@gmail.com>
2026-07-01 18:03:53 +0530

    Partition_arr_accTo_Pivot

--- 2Pointers/Partition_arr_accTo_Pivot.java
+++ 2Pointers/Partition_arr_accTo_Pivot.java
@@ -1,14 +1,17 @@
 public class Partition_arr_accTo_Pivot {
     public static void main(String[] args) {
-        int nums[] = {9,12,5,10,14,3,10}; int pivot = 10;
-        display(pivotArray(nums,pivot));
+        int nums[] = { 9, 12, 5, 10, 14, 3, 10 };
+        int pivot = 10;
+        display(pivotArray(nums, pivot));
 
         System.out.println();
-        
-        int nums1[] = {-3,4,3,2}; int pivot1 = 2;
-        display(pivotArray(nums1,pivot1));
-        
+
+        int nums1[] = { -3, 4, 3, 2 };
+        int pivot1 = 2;
+        display(pivotArray(nums1, pivot1));
+
     }
+
     public static void display(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
@@ -16,27 +19,27 @@ public class Partition_arr_accTo_Pivot {
     }
 
     public static int[] pivotArray(int[] nums, int pivot) {
-        
-        int ans[]= new int[nums.length];
 
-        int idx=0;
-        for(int i : nums){
-            if(i<pivot){
-                ans[idx]= i;
+        int ans[] = new int[nums.length];
+
+        int idx = 0;
+        for (int i : nums) {
+            if (i < pivot) {
+                ans[idx] = i;
                 idx++;
             }
         }
 
-        for(int i : nums){
-            if(i==pivot){
-                ans[idx]= i;
+        for (int i : nums) {
+            if (i == pivot) {
+                ans[idx] = i;
                 idx++;
             }
         }
 
-        for(int i : nums){
-            if(i>pivot){
-                ans[idx]= i;
+        for (int i : nums) {
+            if (i > pivot) {
+                ans[idx] = i;
                 idx++;
             }
         }
