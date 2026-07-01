708b4a95c342f7ba0356ccfdd84a19a76c24c88b
Aman Gaade <ak9070777@gmail.com>
2026-07-01 17:12:07 +0530

    apply operation on arr

--- 2Pointers/Apply_operations_toArray.java
+++ 2Pointers/Apply_operations_toArray.java
@@ -1,29 +1,32 @@
 public class Apply_operations_toArray {
     public static void main(String[] args) {
-       int  nums[] = {1,2,2,1,1,0};
-        int nums1[]={0,1};
-       
-       display(applyOperations(nums));
-       System.out.println();
-       display(applyOperations(nums1));
+        int nums[] = { 1, 2, 2, 1, 1, 0 };
+        int nums1[] = { 0, 1 };
+
+        display(applyOperations(nums));
+        System.out.println();
+        display(applyOperations(nums1));
     }
+
     public static int[] applyOperations(int[] arr) {
-        int i =0;
-        int j = i+1;
+        int i = 0;
+        int j = i + 1;
 
-        while (i<=arr.length-2) {
-            if(arr[i]==arr[j]){
-                arr[i]=arr[i]*2;
-                arr[j]=0;
-                i++; j++;
+        while (i <= arr.length - 2) {
+            if (arr[i] == arr[j]) {
+                arr[i] = arr[i] * 2;
+                arr[j] = 0;
+                i++;
+                j++;
 
-            }
-            else{
-                i++; j++;
+            } else {
+                i++;
+                j++;
             }
         }
         return move(arr);
     }
+
     public static int[] move(int nums[]) {
         int pos = 0;
         for (int i = 0; i < nums.length; i++) {
@@ -40,6 +43,7 @@ public class Apply_operations_toArray {
         return nums;
 
     }
+
     public static void display(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
